84f176cd9942dbd4d64013c8cf7c89998889fb06
mrahman <mrahman@zorin>
2026-03-15 06:41:45 +0100

    chore: bump version to 0.1.2

---
+++ CHANGE.md
@@ -0,0 +1,3 @@
+Release 0.1.2
+
+- feat: Added setting to hide group posts in feed.
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ extensions.configure<ApplicationExtension> {
         applicationId = "com.ycngmn.nobook"
         minSdk = 23
         targetSdk = 36
-        versionCode = 11
-        versionName = "0.1.1"
+        versionCode = 12
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
